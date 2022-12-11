bb44f8a00489984f7bf7b985369fbc54c3843c5d
Morti123 <114169876+Morti123@users.noreply.github.com>
2022-12-11 15:29:28 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <SFML\Graphics\Color.hpp>
+const float w_width = 800;
+const float w_height = 600;
+const std::string w_title = "Work_3";
+const float rize = 50.f;
+const sf::Color Circle_Color(sf::Color::Cyan);
+const float r = 10.f; 
